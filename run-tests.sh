e42d49179cb0b00b3f4a6ab718617af0d16d46df
Eduardo Vasconcelos <59936890+vasconcedu@users.noreply.github.com>
2020-11-28 14:48:00 -0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# Eduardo Vasconcelos (vasconcedu)
-# tereresecurity.wordpress.com
+# Eduardo Vasconcelos (vasconcedu), tereresecurity.wordpress.com
+# Diogo Brandao (dbrandao)
 
 php test.php test/drbgvectors_no_reseed test/drbgvectors_pr_false/ test/drbgvectors_pr_true/
  
