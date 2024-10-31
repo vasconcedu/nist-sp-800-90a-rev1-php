31800333b1543a4a0da77672b7cc6c25b658d529
Eduardo Vasconcelos <59936890+vasconcedu@users.noreply.github.com>
2024-10-31 02:09:02 -0300

    Update run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -1,8 +1,3 @@
 #!/bin/sh
 
-# Authors
-# Eduardo Vasconcelos (vasconcedu), https://vasconcedu.github.io/
-# Diogo Brandao (dbrandao)
-
 php test.php test/drbgvectors_no_reseed test/drbgvectors_pr_false/ test/drbgvectors_pr_true/
- 
